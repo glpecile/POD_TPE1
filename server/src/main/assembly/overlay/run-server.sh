586ca2efc9e0f5f8b707f2bf60a05aedb39d8fac
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-09-14 20:16:25 -0300

    Create run scripts (#99)
    * [Refactor] Added scripts to run client and server
    
    * [Feature] Added the possibility to change the server's rmi port
    
    * [Fix] Stopped deleting old files

--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-PATH_TO_CODE_BASE=`pwd`
+#PATH_TO_CODE_BASE=`pwd`
 
 #JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"	
 
 MAIN_CLASS="ar.edu.itba.pod.server.Server"
 
 
-java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS $*
+java  -cp 'lib/jars/*' $MAIN_CLASS "$@"
+
--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -7,6 +7,9 @@ import ar.edu.itba.pod.server.services.AdminServiceImpl;
 import ar.edu.itba.pod.server.services.NotificationServiceImpl;
 import ar.edu.itba.pod.server.services.SeatAssignmentServiceImpl;
 import ar.edu.itba.pod.server.services.SeatMapServiceImpl;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,9 +21,16 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
+    private static final CommandLineParser parser = new DefaultParser();
+    private static final Options options = new Options().addOption("p","port",true,"Port to listen on");
+
 
     public static void main(String[] args) throws Exception {
         logger.info("tpe1-g3-parent Server Starting ...");
+
+        var cli = parser.parse(options,args);
+        int port = cli.hasOption("port") ? Integer.parseInt(cli.getOptionValue("port")) : 1099;
+
         Map<String, Plane> planes = Collections.synchronizedMap(new HashMap<>());
         Map<String, Flight> flights = Collections.synchronizedMap(new HashMap<>());
         EventsManagerImpl eventsManager = new EventsManagerImpl();
@@ -34,7 +44,7 @@ public class Server {
         var remoteSeatMap = UnicastRemoteObject.exportObject(serviceSeatMap,0);
         var remoteNotification = UnicastRemoteObject.exportObject(serviceNotification,0);
 
-        final Registry registry = LocateRegistry.getRegistry();
+        final Registry registry = LocateRegistry.getRegistry(port);
         registry.rebind("AdminService", remoteAdmin); // bind, rebind, unbind
         registry.rebind("SeatAssignmentService", remoteSeatAssignment);
         registry.rebind("SeatMapService", remoteSeatMap);
